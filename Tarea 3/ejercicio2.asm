06885d61287afc2be6e7bf6986bfa93d315d2fc5
Carlos Estevez <54195505+Cestevez21@users.noreply.github.com>
2021-05-11 21:20:23 -0600

    Agregando ejercicio 2

--- Tarea 3/ejercicio2.asm
+++ Tarea 3/ejercicio2.asm
@@ -1,1 +1,23 @@
-org 100h
+        org 100h
+
+
+section .text
+
+        XOR SI, SI ;Limpiamos variables
+        XOR AX, AX
+        mov AL, 5d
+        mov SI, 4d
+        jmp factorial
+
+factorial:
+        CMP SI, 0d
+        JE asignar
+
+        MUL SI
+        DEC SI
+        jmp factorial
+
+asignar:
+        mov [20Bh], AL
+
+        int     20h
